0afc9f7a785ba93b0bec62716f0d0f2c9e1e0f45
DuyPhamziz <149025650+DuyPhamziz@users.noreply.github.com>
2026-06-20 16:40:16 +0700

    Khoi tao du an

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      message: 'Hello Vue!',
+    }
+  },
+}
+</script>
+
+<template>
+  <div id="app">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  /* -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale; */
+  color: #2c3e50;
+  /* margin-top: 60px; */
+}
+h1 {
+  font-weight: normal;
+  line-height: 1.5;
+  font-size: 2em;
+}
+</style>
